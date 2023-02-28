849e679e35006d6f9a2ab9bf7bd03de59c671cde
Brandonbay <undftdcreed@gmail.com>
2023-02-28 15:56:31 -0800

    2nd draft

--- project_1/2048.js
+++ project_1/2048.js
@@ -1,8 +1,12 @@
+//clarify the board
+//clarify different values of 2048 ie [2,4,8,16,32,64,128,256,512,1024,2048]
+
+
 var board;
 var score;
 var rows = 4;
 var columns = 4;
-
+//when loading the browser up
 window.onload = function() {
    setGame();
 }
@@ -14,18 +18,59 @@ function setGame(){
         [0, 0, 0, 0],
         [0, 0, 0, 0]
     ];
-}
 
+//iterate through the rows and columns and create a coordinate location and a div for each valueuser stor
 for(let r = 0; r < rows; r++) {
     for(let c = 0; c < columns; c++){
         let tile = document.createElement("div");
+        //create a div with an id "0-0"
         tile.id = r.toString() + "-" + c.toString();
         let num = board[r][c];
+        //because were moving the tiles around we call a function to update the style
         updateTile(tile, num);
         document.getElementById("board").append(tile);
-
+        }
     }
-//creates two random spots to place 2 and a 4 at the beginning of the game
     setTwo();
     setTwo();
 }
+
+
+    function updateTile(tile,num) {
+        tile.innerText = "";
+        tile.classList.value = "";
+        //resets the text and class values
+        tile.classList.add("tile");
+    
+    if (num > 0) {
+        tile.innerText = num.toString();
+        if(num <= 4096) {
+            tile.classList.add("x"+num.toString());
+        //update colors if less than or equal to 4096
+        } else {
+            tile.classList.add("x8192");
+        //update to 8192 colors 
+        }
+        }   
+    }
+//creates two random spots to place 2 and a 4 at the beginning of the game
+   function setTwo() {
+    if (!hasEmptyTile()) {
+        return;
+    }
+    let found = false;
+    while (!found) {
+        //find random row and column to place a 2 in
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        if (board[r][c] == 0) {
+            board[r][c] = 2;
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            tile.innerText = "2";
+            //sets to 2
+            tile.classList.add("x2");
+            //sets color class to x2
+            found =  true;
+        }
+    }
+   }
--- project_1/README.md
+++ project_1/README.md
@@ -1,1 +1,18 @@
 1. Project 2048
+------------------------------------------------
+Project one of Unit one
+
+User Story
+---------------
+User will be able to shift tiles in directions to add up variables of the same amount. Variables will then be added up frequently while randomly assigning two new more values to the board. Goal is to get to the number 2048.
+
+MVP features
+
+
+
+
+
+Additional References
+------------------------
+- Wireframe Mockups
+https://www.figma.com/file/r0oVGBIcO8DmN9vdUQKSIK/2048?node-id=0%3A1&t=UPWgWdKPBjdbIOUQ-0
